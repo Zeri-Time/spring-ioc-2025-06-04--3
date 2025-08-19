3bd6c4eb1c7df13f0d5d2d85feee8d01d4cdb924
SWWWin <bgpd1258@naver.com>
2025-08-19 17:39:19 +0900

    feat: t1, t2, t3, t4, t5, t6 구현

--- src/main/java/com/ll/framework/ioc/ApplicationContext.java
+++ src/main/java/com/ll/framework/ioc/ApplicationContext.java
@@ -1,14 +1,63 @@
 package com.ll.framework.ioc;
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.ll.framework.ioc.annotations.Repository;
+import com.ll.framework.ioc.annotations.Service;
+import com.ll.standard.util.Ut;
+import org.reflections.Reflections;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class ApplicationContext {
+    private final Map<String, Object> beans = new HashMap<>();
+
     public ApplicationContext(String basePackage) {
+        Reflections reflections = new Reflections(basePackage);
+
+
+        Set<Class<?>> repositories = reflections.getTypesAnnotatedWith(Repository.class);
+        for (Class<?> repository : repositories) {
+            try {
+                String beanName = Ut.str.lcfirst(repository.getSimpleName());
+                beans.put(beanName, repository.getDeclaredConstructor().newInstance());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+
+
+        Set<Class<?>> services = reflections.getTypesAnnotatedWith(Service.class);
+        for (Class<?> service : services) {
+            try {
+                String beanName = Ut.str.lcfirst(service.getSimpleName());
+
+                var constructor = service.getDeclaredConstructors()[0];
+                var paramTypes = constructor.getParameterTypes();
+                Object[] params = new Object[paramTypes.length];
+
+                for (int i = 0; i < paramTypes.length; i++) {
+                    String depName = Ut.str.lcfirst(paramTypes[i].getSimpleName());
+                    params[i] = beans.get(depName);
+                }
+
+                beans.put(beanName, constructor.newInstance(params));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
 
     }
 
     public void init() {
+
     }
 
+    @SuppressWarnings("unchecked")
     public <T> T genBean(String beanName) {
-        return null;
+        return (T) beans.get(beanName);
     }
 }
